1057b61d29a2f83ae8da199153c0b38fbf35ac22
이주희 <95258339+VeryverySoHappy@users.noreply.github.com>
2024-08-22 18:10:38 +0900

    id success publishing

---
+++ client/src/pages/IdSuccess.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Logo from "../assets/img/logo_broom.svg";
+
+function IdSuccess() {
+  return (
+    <section className="id-success">
+      <div className="text">
+        <img src={Logo} alt="B.ROOM 로고" className="text__logo" />
+        <p className="text__title">
+          회원님의 아이디는<br/><strong>ID</strong>입니다.
+        </p>
+        <p className="text__desc">아이디 찾기가 완료되었습니다!</p>
+      </div>
+
+      <div className="btn">
+        <Link to="/" className="btn__home">홈으로</Link>
+        <Link to="/login" className="btn__login">로그인</Link>
+      </div>
+    </section>
+  );
+}
+
+export default IdSuccess
