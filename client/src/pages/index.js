73e4340dc2e3276edf20161e7547d34c486a1dbe
이주희 <95258339+VeryverySoHappy@users.noreply.github.com>
2024-08-26 11:39:22 +0900

    jsx 파일 연결

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,5 +1,18 @@
+// layout
+export { default as Header } from '../components/Header';
+export { default as Nav} from '../components/Nav';
+
+// pages
 export { default as HomeLayout } from './HomeLayout';
 export { default as Error } from './Error';
 export { default as Landing } from './Landing';
 export { default as Login } from './Login';
 export { default as Profile } from './Profile';
+
+// my pages
+export { default as Join } from './Join';
+export { default as JoinSuccess } from './JoinSuccess';
+export { default as Id } from './Id';
+export { default as IdSuccess } from './IdSuccess';
+export { default as Pw } from './Pw';
+export { default as PwSuccess } from './PwSuccess';
