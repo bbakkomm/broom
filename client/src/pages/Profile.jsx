e741b51be14df5c26b8237f86bcfb698efae801a
keemdb <keemdb@gmail.com>
2024-08-28 17:49:18 +0900

    프로필 작업중

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,8 +1,32 @@
+
+import '../assets/scss/profile/profile.scss';
+// import Stack from '@mui/material/Stack';
+// import Button from '@mui/material/Button';
+import SearchIcon from '@mui/icons-material/Search';
+
+
 const Profile = () => {
+
+  const imgPath = '/src/assets/img/profile/'
+  let profileThumb = 'profile_01.png'
+
   return (
-    <div>
-      <h1>Profile Page</h1>
-      <p>test david</p>
+    <div className="profile">
+      <div className="inner_360">
+        <header className="profile-header">
+          <h3 className="profile-header__title">마이페이지</h3>
+          <SearchIcon className="btn_search"/>
+        </header>
+        <div className="profile-info">
+          <div className="img-box profile-info__thumb">
+            <img src={imgPath + profileThumb}/>
+          </div>
+          <div className="profile">
+
+          </div>
+
+        </div>
+      </div>
     </div>
   )
 }
