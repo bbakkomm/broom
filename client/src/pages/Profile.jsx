edb52255f2b883ab8afec8b2f2d333ae9b3b0f7e
keemdb <keemdb@gmail.com>
2024-08-29 16:39:04 +0900

    프로필 카드 완료

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -27,8 +27,34 @@ const Profile = () => {
             <p className="profile-card__email">broom@broom.com</p>
           </div>
           <ArrowForwardIosIcon className="btn_arrowforward"/>
-
         </div>
+          <ul className="skill-tag-box">
+            <li className="skill-tag-box__item">
+              <span className="skill-tag skill-tag--js">JavaScript</span>
+            </li>
+            <li>
+              <span className="skill-tag skill-tag--ts">TypeScript</span>
+            </li>
+            <li>
+              <span className="skill-tag skill-tag--react">React</span>
+            </li>
+          </ul>
+          <div className="status-box">
+            <ul className='status'>
+              <li className="status__item">
+                <span className="status__info status__info--like">999+</span>
+                <span className="status__desc">찜모임</span>
+              </li>
+              <li className="status__item">
+                <span className="status__info status__info--studing">2</span>
+                <span className="status__desc">활동중</span>
+              </li>
+              <li className="status__item">
+                <span className="status__info status__info--complate">3</span>
+                <span className="status__desc">완료</span>
+              </li>
+            </ul>
+          </div>
       </div>
     </div>
   )
