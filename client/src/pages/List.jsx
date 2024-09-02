0a70b4ea307e8fe1a0d748d151fe5e043c7ddddd
godongkyu <4skadl@naver.com>
2024-09-02 10:16:04 +0900

    수정

--- client/src/pages/List.jsx
+++ client/src/pages/List.jsx
@@ -1,9 +1,43 @@
+import React, { useState } from "react";
 import ListWhole from "./ListWhole"
+import Nav from "../components/Nav";
 
 const List = () => {
+
+  //전체, 모집중, 완료
+  const [selectedIndex, setSelectedIndex] = useState(null);
+  const handleClick = (index, e) => {
+    e.preventDefault();
+    setSelectedIndex(index);
+  }
     return (
       <div>
+        {/* <Header></Header> */}
+        <div className="list_tit">
+          <a href="" onClick={(e) => handleClick(0, e)}>
+            <div style={{
+            color: selectedIndex === 0 ? 'black' : 'black',
+            fontWeight: selectedIndex === 0 ? 'bold' : 'normal',
+            }}>전체</div>
+          </a>
+
+          <a href="" onClick={(e) => handleClick(1, e)}>
+            <div style={{
+            color: selectedIndex === 1 ? 'black' : 'black',
+            fontWeight: selectedIndex === 1 ? 'bold' : 'normal',
+            }}>모집중</div>
+          </a>
+
+          <a href="" onClick={(e) => handleClick(2, e)}>
+            <div style={{
+            color: selectedIndex === 2 ? 'black' : 'black',
+            fontWeight: selectedIndex === 2 ? 'bold' : 'normal',
+            }}>완료</div>
+          </a>
+        </div>
+
         <ListWhole/>
+        <Nav/>
       </div>
     )
   }
