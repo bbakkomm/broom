5c7032369486de3b9103e7f3a4ca1daa33db58b0
sunghun <bbakkomm@gmail.com>
2024-09-18 18:12:22 +0900

    회원가입 경로 수정

--- client/src/pages/register/Join.jsx
+++ client/src/pages/register/Join.jsx
@@ -12,7 +12,7 @@ export const action = async ({ request }) => {
   try {
     await customFetch.post('/auth/register', data);
     toast.success('register successful');
-    return redirect('/');
+    return redirect('/joinsuccess');
   } catch (error) {
     toast.error(error?.response?.data?.msg);
     return error;
