34a79ef51687d4c58576bcabb237c42419e03caa
godongkyu <4skadl@naver.com>
2024-09-24 11:32:09 +0900

    리스트

--- client/src/pages/List.jsx
+++ client/src/pages/List.jsx
@@ -14,40 +14,27 @@ const List = () => {
       <div>
         {/* <Header></Header> */}
         <div className="list_tit">
-          <a href="" onClick={(e) => handleClick(0, e)}>
+          <a href="/whole" onClick={(e) => handleClick(0, e)}>
             <div style={{
             color: selectedIndex === 0 ? 'black' : 'black',
             fontWeight: selectedIndex === 0 ? 'bold' : 'normal',
             }}>전체</div>
           </a>
 
-          <a href="" onClick={(e) => handleClick(1, e)}>
+          <a href="/assemble" onClick={(e) => handleClick(1, e)}>
             <div style={{
             color: selectedIndex === 1 ? 'black' : 'black',
             fontWeight: selectedIndex === 1 ? 'bold' : 'normal',
             }}>모집중</div>
           </a>
 
-          <a href="" onClick={(e) => handleClick(2, e)}>
+          <a href="/complete" onClick={(e) => handleClick(2, e)}>
             <div style={{
             color: selectedIndex === 2 ? 'black' : 'black',
             fontWeight: selectedIndex === 2 ? 'bold' : 'normal',
             }}>완료</div>
           </a>
         </div>
-        {
-          function select(){
-            // if(selectedIndex === 0){
-
-            // }else if(selectedIndex === 1){
-
-            // }else if (selectedIndex === 2){
-              
-            // }
-
-            console.log(selectedIndex === 1);
-          }
-        }
         <ListWhole/>
         <Nav/>
       </div>
