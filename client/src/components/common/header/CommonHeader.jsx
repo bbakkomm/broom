694ef635a439f44170f34c4a5a34e3ed33aa5406
VeryverySoHappy <frontEnd0815@gmail.com>
2024-09-26 09:55:55 +0900

    좋아요 버튼, 공유하기 버튼 컴포넌트 작업

--- client/src/components/common/header/CommonHeader.jsx
+++ client/src/components/common/header/CommonHeader.jsx
@@ -5,31 +5,15 @@ import styles from "./CommonHeader.module.scss"
 
 // icon
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
-import FavoriteOutlinedIcon from "@mui/icons-material/FavoriteOutlined";
-import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
 import ArrowBackIosNewOutlinedIcon from "@mui/icons-material/ArrowBackIosNewOutlined";
-import ShareOutlinedIcon from "@mui/icons-material/ShareOutlined";
 import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
 import Logo from "../../../assets/img/common/logo_broom.svg";
-import { useState } from 'react';
+import HeartBtn from './component/HeartBtn';
+import ShareBtn from './component/ShareBtn';
   
 function Header(props) {
   const { home, logo, search, rightBtn, to, title, user, study } = props;
-  const [heart, setHeart] = useState(false);
-  const [heartCount, setHeartCount] = useState(0);
   const navigate = useNavigate();
-
-  // 좋아요버튼 클릭시 실행되는 함수
-  const handleHeart = () => {
-    if(heart) {
-      setHeartCount(heartCount + 1);
-    } else {
-      setHeartCount(heartCount - 1);
-    }
-    setHeart(!heart);
-  }
-
-  
 
   return (
     <header className={styles.header}>
@@ -83,7 +67,7 @@ function Header(props) {
                     <h1 className={styles.header__title}>{title}</h1>
                   </Link>
                   <div className={styles.header__btn}>
-                    <button><ShareOutlinedIcon /></button>
+                    <ShareBtn/>
                     { study ? <Link to="/study/studycreation"><SettingsOutlinedIcon /></Link> : <Link to="/project/projectcreation"><SettingsOutlinedIcon /></Link>}
                   </div>
                 </>
@@ -95,10 +79,8 @@ function Header(props) {
                     <h1 className={styles.header__title}>{title}</h1>
                   </Link>
                   <div className={styles.header__btn}>
-                    <button className={styles.header__heart} onClick={handleHeart}>
-                      {heart ? <FavoriteOutlinedIcon className={styles.heart__icon}/> : <FavoriteBorderOutlinedIcon className={styles.heart__icon}/>}
-                    </button>
-                    <button><ShareOutlinedIcon  className={styles.heart__icon}/></button>
+                    <HeartBtn/>
+                    <ShareBtn/>
                   </div>
                 </>
               )}
