aba7561efd795ccd5647278ad31f42bbe4c11869
VeryverySoHappy <frontEnd0815@gmail.com>
2024-09-26 11:36:37 +0900

    register 페이지들 scss 수정

--- client/src/pages/register/IdSuccess.jsx
+++ client/src/pages/register/IdSuccess.jsx
@@ -7,15 +7,17 @@ function IdSuccess() {
 
   return (
     <main className="success">
-      <div className="text">
-        <img src={Logo} alt="B.ROOM 로고" className="text__logo" />
-        <p className="text__title">회원님의 아이디는<br /><strong>{uid}</strong> 입니다.</p>
-        <p className="text__desc">아이디 찾기가 완료되었습니다!</p>
+      <div className="success__inner">
+        <div className="text">
+          <img src={Logo} alt="B.ROOM 로고" className="text__logo" />
+          <p className="text__title">회원님의 아이디는<br /><strong>{uid}</strong> 입니다.</p>
+          <p className="text__desc">아이디 찾기가 완료되었습니다!</p>
+        </div>
       </div>
 
       <div className="btn">
-        <Link to="/" className="btn-outline">홈으로</Link>
-        {/* <Link to="/login" className="btn-bg">로그인</Link> */}
+        <Link to="/home" className="btn-outline">홈으로</Link>
+        <Link to="/login" className="btn-bg">로그인</Link>
       </div>
     </main>
   );
--- client/src/pages/register/JoinSuccess.jsx
+++ client/src/pages/register/JoinSuccess.jsx
@@ -5,15 +5,17 @@ import Logo from "../../assets/img/common/logo_broom.svg";
 const JoinSuccess = () => {
   return (
     <main className="success">
-      <div className="text">
-        <img src={Logo} alt="B.ROOM 로고" className="text__logo" />
-        <p className="text__title"><strong>회원가입</strong>이<br />완료되었습니다.</p>
-        <p className="text__desc">B.ROMM에 오신 것을 환영합니다!</p>
+      <div className="success__inner">
+        <div className="text">
+          <img src={Logo} alt="B.ROOM 로고" className="text__logo" />
+          <p className="text__title"><strong>회원가입</strong>이<br />완료되었습니다.</p>
+          <p className="text__desc">B.ROMM에 오신 것을 환영합니다!</p>
+        </div>
       </div>
 
       <div className="btn">
-        {/* <Link to="/" className="btn-outline">홈으로</Link> */}
-        <Link to="/" className="btn-bg">로그인</Link>
+        <Link to="/home" className="btn-outline">홈으로</Link>
+        <Link to="/login" className="btn-bg">로그인</Link>
       </div>
     </main>
   );
