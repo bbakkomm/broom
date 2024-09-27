95e201ecc964c6c28fc91a9d6d9359e003bd0a5e
VeryverySoHappy <frontEnd0815@gmail.com>
2024-09-27 19:57:15 +0900

    className 수정

--- client/src/components/common/navigation/CommonNav.jsx
+++ client/src/components/common/navigation/CommonNav.jsx
@@ -1,8 +1,5 @@
 import { NavLink } from "react-router-dom";
 
-// css
-import styles from "./CommonNav.module.scss"
-
 // icon
 import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
 import MenuBookOutlinedIcon from '@mui/icons-material/MenuBookOutlined';
@@ -11,38 +8,41 @@ import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined
 
 function Nav(props) {
   return (
-    <nav className={styles.nav}>
-      <ul className={styles.nav__list}>
-        <li className={styles.nav__item}>
-          <NavLink to="/" className={({ isActive }) => (isActive ? styles.nav__linkOn : styles.nav__link)}>
+    <nav className="nav">
+      <ul className="nav__list">
+        <li className="nav__item">
+          <NavLink 
+            to="/" 
+            className={({ isActive }) => (isActive ? "nav__linkOn" : "nav__link")}
+          >
             <HomeOutlinedIcon/>
-            <p className={styles.nav__btn}>홈</p>
+            <p className="nav__btn">홈</p>
           </NavLink>
         </li>
-        <li className={styles.nav__item}>
+        <li className="nav__item">
           <NavLink
             to="/study"
-            className={({ isActive }) => (isActive ? styles.nav__linkOn : styles.nav__link)}
+            className={({ isActive }) => (isActive ? "nav__linkOn" : "nav__link")}
           >
             <MenuBookOutlinedIcon />
-            <p className={styles.nav__btn}>스터디</p>
+            <p className="nav__btn">스터디</p>
           </NavLink>
         </li>
-        <li className={styles.nav__item}>
+        <li className="nav__item">
           <NavLink
             to="/studycreation"
-            className={({ isActive }) => (isActive ? styles.nav__linkOn : styles.nav__link)}
+            className={({ isActive }) => (isActive ? "nav__linkOn" : "nav__link")}
           >
             <AddIcon />
-            <p className={styles.nav__btn}>스터디 생성</p>
+            <p className="nav__btn">스터디 생성</p>
           </NavLink>
         </li>
-        <li className={styles.nav__item}>
+        <li className="nav__item">
           <NavLink
             to="/profile"
-            className={({ isActive }) => (isActive ? styles.nav__linkOn : styles.nav__link)}>
+            className={({ isActive }) => (isActive ? "nav__linkOn" : "nav__link")}>
             <PersonOutlineOutlinedIcon />
-            <p className={styles.nav__btn}>마이</p>
+            <p className="nav__btn">마이</p>
           </NavLink>
         </li>
       </ul>
