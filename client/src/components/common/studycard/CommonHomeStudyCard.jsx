682fce7b8bcab373ed79658bd02a787273a903b1
VeryverySoHappy <frontEnd0815@gmail.com>
2024-09-29 22:14:04 +0900

    홈 img, style

--- client/src/components/common/studycard/CommonHomeStudyCard.jsx
+++ client/src/components/common/studycard/CommonHomeStudyCard.jsx
@@ -1,6 +1,3 @@
-// css
-import styles from './CommonHomeStudyCard.module.scss';
-
 function HomeStudyCard(
   { 
     idx, 
@@ -27,69 +24,48 @@ function HomeStudyCard(
             <div className="studyBox__image">
               <img src={imgPath + thumb} alt={title + " 모임 상세보기"} />
             </div>
-            <h3 className="studyBox__title">{title}</h3>
-
-            {/* <ul className={styles.studyBox__list}>
-              <li className={styles.studyBox__list__item}>
-                <CalendarTodayRoundedIcon />
-                <span className={styles.studyBox__list__desc}>{date} ~</span>
-              </li>
-              <li className={styles.studyBox__list__item}>
-                <AccessAlarmsRoundedIcon />
-                <span className={styles.studyBox__list__desc}>{time}</span>
-              </li>
-              <li className={styles.studyBox__list__item}>
-                <PlaceOutlinedIcon />
-                <span className={styles.studyBox__list__desc}>{place}</span>
-              </li>
-              <li className={styles.studyBox__list__item}>
-                <PaymentsOutlinedIcon />
-                <span className={styles.studyBox__list__desc}>{price}</span>
-              </li>
-              <li className={styles.studyBox__list__item}>
-                <PeopleAltOutlinedIcon />
-                <span className={styles.studyBox__list__desc}>최소 {minimumPerson}명 ~ 최대 {maximumPerson}명</span>
-              </li>
-            </ul> */}
+            <div className="studyBox__text">
+              <h3 className="studyBox__title">{title}</h3>
+              <div className="skill-tag">
+                {complate ? <span className="skill-tag--complate">완료</span> : ""}
+                {skillTag.includes("javascript") ? (
+                  <span className="skill-tag--javascript">JavaScript</span>
+                ) : (
+                  ""
+                )}
+                {skillTag.includes("typescript") ? (
+                  <span className="skill-tag--typescript">TypeScript</span>
+                ) : (
+                  ""
+                )}
+                {skillTag.includes("react") ? (
+                  <span className="skill-tag--react">React</span>
+                ) : (
+                  ""
+                )}
+                {skillTag.includes("dart") ? (
+                  <span className="skill-tag--dart">Dart</span>
+                ) : (
+                  ""
+                )}
+                {skillTag.includes("flutter") ? (
+                  <span className="skill-tag--flutter">Flutter</span>
+                ) : (
+                  ""
+                )}
+                {skillTag.includes("html") ? (
+                  <span className="skill-tag--html">HTML</span>
+                ) : (
+                  ""
+                )}
+                {skillTag.includes("css") ? (
+                  <span className="skill-tag--css">CSS</span>
+                ) : (
+                  ""
+                )}
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="skill-tag">
-          {complate ? <span className="skill-tag--complate">완료</span> : ""}
-          {skillTag.includes("javascript") ? (
-            <span className="skill-tag--javascript">JavaScript</span>
-          ) : (
-            ""
-          )}
-          {skillTag.includes("typescript") ? (
-            <span className="skill-tag--typescript">TypeScript</span>
-          ) : (
-            ""
-          )}
-          {skillTag.includes("react") ? (
-            <span className="skill-tag--react">React</span>
-          ) : (
-            ""
-          )}
-          {skillTag.includes("dart") ? (
-            <span className="skill-tag--dart">Dart</span>
-          ) : (
-            ""
-          )}
-          {skillTag.includes("flutter") ? (
-            <span className="skill-tag--flutter">Flutter</span>
-          ) : (
-            ""
-          )}
-          {skillTag.includes("html") ? (
-            <span className="skill-tag--html">HTML</span>
-          ) : (
-            ""
-          )}
-          {skillTag.includes("css") ? (
-            <span className="skill-tag--css">CSS</span>
-          ) : (
-            ""
-          )}
         </div>
       </li>
     </ul>
