ddfd8731c616c7b907f426ce3cc000fa7032a484
sunghun <bbakkomm@gmail.com>
2024-10-02 10:25:53 +0900

    스터디 원복

--- client/src/components/common/studycard/CommonStudyCard.jsx
+++ client/src/components/common/studycard/CommonStudyCard.jsx
@@ -11,30 +11,47 @@ import PeopleAltOutlinedIcon from "@mui/icons-material/PeopleAltOutlined";
 import FavoriteOutlinedIcon from "@mui/icons-material/FavoriteOutlined";
 import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
 
-function StudyCard({ item }) {
+function StudyCard(
+  { 
+    studyType,
+    idx, 
+    objId,
+    title, 
+    thumb,
+    startDate, 
+    endDate, 
+    time, 
+    place, 
+    price, 
+    minimumPerson, 
+    maximumPerson,
+    skillTag,
+    complete,
+    like
+  }
+) {
   const currentLocation = useLocation().pathname;
   const navigate = useNavigate();
-
   const loadData = useLoaderData();
 
   let studys = []
   let currentUserId = '';
 
-  if (item.studyType === 'all') {
+  if (studyType === 'all') {
     studys = loadData[0].studys;
     currentUserId = loadData[1].data.user._id;
-  } else if (item.studyType === 'like') {
+  } else if (studyType === 'like') {
     currentUserId = loadData[0].user._id;
     studys = loadData[1].studys;
-  } else if (item.studyType === 'study') {
+  } else if (studyType === 'study') {
     currentUserId = loadData[0].user._id;
     studys = loadData[2].studys;
   }
   
-  const getCurrentStudy = studys.find(v => v._id === item.objId);
+  const getCurrentStudy = studys.find(v => v._id === objId);
   // let likes = like;
-  // let likeArr = getCurrentStudy.like;
-  // let isLike = likeArr.includes(currentUserId);
+  let likeArr = getCurrentStudy.like;
+  let isLike = likeArr.includes(currentUserId);
 
   const listClickHandler = (e) => {
     e.preventDefault();
@@ -69,107 +86,103 @@ function StudyCard({ item }) {
   }
 
   return (
-    <div>
-      {item.map((item, idx)=>(
-        <ul className="studycard" key={idx} data-prod={item.objId} onClick={console.log('click')}>
-          <li className="studycard__item">
-            <div className="skill-tag">
-              {
-                item.complete
-                ? (<span className="skill-tag__complate">완료</span>)
-                : (<span className="skill-tag__ing">모집중</span>)
-              }
-              {
-                  item.skillTag.includes('javascript')
-                  ? (<span className="skill-tag__javascript">JavaScript</span>)
-                  : ''
-              }
-              {
-                  item.skillTag.includes('react')
-                  ? (<span className="skill-tag__react">React</span>)
-                  : ''
-              }
-              {
-                  item.skillTag.includes('vue')
-                  ? (<span className="skill-tag__vue">Vue</span>)
-                  : ''
-              }
-              {
-                  item.skillTag.includes('typescript')
-                  ? (<span className="skill-tag__typescript">TypeScript</span>)
-                  : ''
-              }
-              {
-                  item.skillTag.includes('dart')
-                  ? (<span className="skill-tag__dart">Dart</span>)
-                  : ''
-              }
-              {
-                  item.skillTag.includes('flutter')
-                  ? (<span className="skill-tag__flutter">Flutter</span>)
-                  : ''
-              }
-              {
-                  item.skillTag.includes('html')
-                  ? (<span className="skill-tag__html">HTML</span>)
-                  : ''
-              }
-              {
-                  item.skillTag.includes('css')
-                  ? (<span className="skill-tag__css">CSS</span>)
-                  : ''
-              }
-              {
-                  item.skillTag.includes('scss')
-                  ? (<span className="skill-tag__scss">SCSS</span>)
-                  : ''
-              }
-              {
-                  item.skillTag.includes('figma')
-                  ? (<span className="skill-tag__figma">Figma</span>)
-                  : ''
-              }
+    <ul className="studycard" key={idx} data-prod={objId} onClick={listClickHandler}>
+      <li className="studycard__item">
+        <div className="skill-tag">
+          {
+            complete
+            ? (<span className="skill-tag__complate">완료</span>)
+            : (<span className="skill-tag__ing">모집중</span>)
+          }
+          {
+              skillTag.includes('javascript')
+              ? (<span className="skill-tag__javascript">JavaScript</span>)
+              : ''
+          }
+          {
+              skillTag.includes('react')
+              ? (<span className="skill-tag__react">React</span>)
+              : ''
+          }
+          {
+              skillTag.includes('vue')
+              ? (<span className="skill-tag__vue">Vue</span>)
+              : ''
+          }
+          {
+              skillTag.includes('typescript')
+              ? (<span className="skill-tag__typescript">TypeScript</span>)
+              : ''
+          }
+          {
+              skillTag.includes('dart')
+              ? (<span className="skill-tag__dart">Dart</span>)
+              : ''
+          }
+          {
+              skillTag.includes('flutter')
+              ? (<span className="skill-tag__flutter">Flutter</span>)
+              : ''
+          }
+          {
+              skillTag.includes('html')
+              ? (<span className="skill-tag__html">HTML</span>)
+              : ''
+          }
+          {
+              skillTag.includes('css')
+              ? (<span className="skill-tag__css">CSS</span>)
+              : ''
+          }
+          {
+              skillTag.includes('scss')
+              ? (<span className="skill-tag__scss">SCSS</span>)
+              : ''
+          }
+          {
+              skillTag.includes('figma')
+              ? (<span className="skill-tag__figma">Figma</span>)
+              : ''
+          }
+        </div>
+        <div className="study-box">
+          <h3 className="study-box__title">{title}</h3>
+          <div className="study-box__content">
+            <div className="study-box__image">
+              <img src={thumb} alt={title + ' 모임 상세보기'} />
             </div>
-            <div className="study-box">
-              <h3 className="study-box__title">{item.title}</h3>
-              <div className="study-box__content">
-                <div className="study-box__image">
-                  <img src={item.thumb} alt={item.title + ' 모임 상세보기'} />
-                </div>
-                <ul className="study-box__list">
-                  <li className="study-box__item">
-                    <CalendarTodayRoundedIcon />
-                    <span className="study-box__desc">{item.startDate} ~ {item.endDate}</span>
-                  </li>
-                  <li className="study-box__item">
-                    <AccessAlarmsRoundedIcon />
-                    <span className="study-box__desc">{item.time}</span>
-                  </li>
-                  <li className="study-box__item">
-                    <PlaceOutlinedIcon />
-                    <span className="study-box__desc">{item.place}</span>
-                  </li>
-                  <li className="study-box__item">
-                    <PaymentsOutlinedIcon />
-                    <span className="study-box__desc">{Number(item.price).toLocaleString('ko-KR')}</span>
-                  </li>
-                  <li className="study-box__item">
-                    <PeopleAltOutlinedIcon />
-                    <span className="study-box__desc">최소 {item.minimumPerson}명 ~ 최대 {item.maximumPerson}명</span>
-                  </li>
-                </ul>
-              </div>
-              {/* <button className="btn-like" onClick={likeClickHandler}>
-                {isLike 
-                  ? <FavoriteOutlinedIcon style={{color: '#FB744A', width:'2.2rem', height: '2rem'}} /> 
-                  : <FavoriteBorderOutlinedIcon style={{color: '#FB744A', width:'2.2rem', height: '2rem'}} />
-                }
-              </button> */}
-            </div>
-          </li>
-        </ul>
-      ))}
-    </div>
+            <ul className="study-box__list">
+              <li className="study-box__item">
+                <CalendarTodayRoundedIcon />
+                <span className="study-box__desc">{startDate} ~ {endDate}</span>
+              </li>
+              <li className="study-box__item">
+                <AccessAlarmsRoundedIcon />
+                <span className="study-box__desc">{time}</span>
+              </li>
+              <li className="study-box__item">
+                <PlaceOutlinedIcon />
+                <span className="study-box__desc">{place}</span>
+              </li>
+              <li className="study-box__item">
+                <PaymentsOutlinedIcon />
+                <span className="study-box__desc">{Number(price).toLocaleString('ko-KR')}</span>
+              </li>
+              <li className="study-box__item">
+                <PeopleAltOutlinedIcon />
+                <span className="study-box__desc">최소 {minimumPerson}명 ~ 최대 {maximumPerson}명</span>
+              </li>
+            </ul>
+          </div>
+          <button className="btn-like" onClick={likeClickHandler}>
+            {isLike 
+              ? <FavoriteOutlinedIcon style={{color: '#FB744A', width:'2.2rem', height: '2rem'}} /> 
+              : <FavoriteBorderOutlinedIcon style={{color: '#FB744A', width:'2.2rem', height: '2rem'}} />
+            }
+          </button>
+        </div>
+      </li>
+    </ul>
   )
 }
 
--- client/src/pages/Study.jsx
+++ client/src/pages/Study.jsx
@@ -7,75 +7,89 @@ import customFetch from "../utils/customFetch.js";
 import SearchBtn from '../components/common/header/component/SearchBtn.jsx';
 
 import StudyCard from '../components/common/studycard/CommonStudyCard';
-import SearchNotFound from '../components/common/studycard/SearchNotFound';
-import { compareSync } from 'bcryptjs';
-import MenuButtons from '../components/common/studycard/MenuButtons';
-import Total from '../components/common/studycard/Total';
+import SearchNotFound from '../components/common/studycard/SearchNotFound.jsx';
 
 export const loader = async ({ req }) => {
 	try {
-	  const resUser = await customFetch.get('/users/current-user', req);
-	  const res = await customFetch.get('/study', req);
-	  return [res.data, resUser];
+		const resUser = await customFetch.get('/users/current-user', req);
+		const res = await customFetch.get('/study', req);
+		return [res.data, resUser];
 	} catch (error) {
-	  console.log(error);
-	  return redirect('/login');
+		console.log(error);
+		return redirect('/login');
 	}
 }
 
-
 function Study() {
 	const loadData = useLoaderData();
-	const { studys } = loadData[0];
-	const currentUserId = loadData[1].data.user._id;
-	// const isLikes = studys.map(v => v.like.includes(currentUserId));
+	let { studys } = loadData[0];
 
-	const [item, setItems] = useState(studys);
-	const menuItems = [...new Set(studys.map((item)=>{ return item.price}))]
+	studys = [...studys].reverse();
 
-	const filterItems = () => {
-		const newItems = studys.filter((item)=> item.complete === true)
-		setItems(newItems)
-	}
-	const filterItems2 = () => {
-		const newItems = studys.filter((item)=> item.complete === false)
-		setItems(newItems)
-	}
-
+	const [studyCard, setStudyCard] = useState(studys);
 	const [search, setSearch] = useState('');
+	const [studyTab, setStudyTab] = useState('all');
+
+	const studyList = studyCard.map((item, idx)=>{
+		return (
+			<StudyCard 
+				studyType={'all'}
+				key={`study_${idx}`}
+				objId={item._id}
+				idx={idx}
+				title={item.title}
+				thumb={item.thumb}
+				startDate={item.startDate}
+				endDate={item.endDate}
+				time={item.time}
+				place={item.place}
+				price={item.price}
+				minimumPerson={item.minimumPerson}
+				maximumPerson={item.maximumPerson}
+				skillTag={item.skillTag}
+				complete={item.complete}
+				status={item.status}
+				name={item.name}
+				location={item.loaction}
+				cost={item.cost}
+				like={item.like}
+			/>      
+		)
+	});
 
 	useEffect(()=>{
 		const filter = studys.filter((item)=>{
-		  return item.title.toLowerCase().includes(search.toLowerCase())
-		})
-		setItems(filter);
-	  }, [search])
+			const searchValue = item.title.toLowerCase().includes(search.toLowerCase());
 
-	  const titleChange = (e)=>{
+			if (studyTab === 'open') {
+				return !item.complete && searchValue;
+			} else if (studyTab === 'close') {
+				return item.complete && searchValue;
+			}
+
+			return searchValue;
+		})
+		setStudyCard(filter);
+	}, [search, studyTab])
+
+	const titleChange = (e)=>{
 		setSearch(e.target.value)
-	  }
-  
+	}
+
   return (
 	<div className="study-wrap">
 		<div className="search-box">
 			<SearchBtn/>
 			<input type="text" value={search} placeholder="원하는 스터디를 찾아보세요!" onChange={titleChange} className="search-box__input" />
 		</div>
-		<MenuButtons 
-			menuItems={menuItems}
-			filterItems={filterItems}
-			filterItems2={filterItems2}
-			setItems={setItems}
-		/>
-		<div className="study-header">
-			<div className="study-header__length">
-				<span className="study-header__length--desc">전체 </span>
-				<Total item={item}/>
-				<span className="study-header__length--desc">개</span>
-			</div>
+		<div className="studytab">
+			<button className={'studytab-btn' + (studyTab === "all" ? " active" : "")} onClick={()=>{setStudyTab("all")}}>전체</button>
+			<button className={'studytab-btn' + (studyTab === "open" ? " active" : "")} onClick={()=>{setStudyTab("open")}}>모집중</button>
+			<button className={'studytab-btn' + (studyTab === "close" ? " active" : "")} onClick={()=>{setStudyTab("close")}}>완료</button>
 		</div>
-		
-		{item.length === 0 ? <SearchNotFound /> : <StudyCard item={item}/>}
+		{
+			studyCard.length === 0 ? <SearchNotFound /> : studyList
+		}
 	</div>
   )
 }
